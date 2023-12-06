27f96894772ade1c6578dfb3037403d28250c55b
hawariyaw paulos <hawipaul308@gmail.com>
2023-12-06 08:17:47 -0800

    register forum

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define("HOST", "localhost");
+define("DB", "forum");
+define("USER", "root");
+define("PASSWORD", "");
+
+try{
+$conn = new PDO("mysql:host=".HOST.";dbname=".DB."",USER,PASSWORD);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "connection successful";
+}
+catch(PDOException $error){
+  echo $error->getMessage();
+}
+?>
